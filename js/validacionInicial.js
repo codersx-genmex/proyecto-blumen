4c7c3f12c26fd8d7cf52e3401c8e2d188563c40a
Ingrid Fonoy <ingridsfonoyd@gmail.com>
2023-03-09 00:49:43 -0600

    formulariosReactivosCambios

--- js/validacionInicial.js
+++ js/validacionInicial.js
@@ -21,20 +21,60 @@ btnContadorResta.addEventListener("click", () => {
 // TERMINA FUNCION BOTON CONTADOR
 
 //INICIA FUNCION NAME FORM
-const form = document.querySelector('#pagoForm');
+const form = document.querySelector( "#pagoForm" );
 
-form.addEventListener('submit', function(event) {
-event.preventDefault();
 
-const name = document.querySelector('#nombre').value.trim();
-//const email = document.querySelector('#email').value.trim();
+form.addEventListener( "submit", ( event ) => {
+    event.preventDefault();
+    const name = document.querySelector( "#nameInput" ).value; 
+    const nameInput = document.querySelector( "#nameInput" );
+    
+    if ( name === '' || name.value == null ) {
+        setErrorFor(nameInput, 'No lo dejes en blanco mijo');
+	} else {
+		setSuccessFor(nameInput);
+    }
+
+    /*if ( messages.length > 0 ) {
+        e.preventDefault();
+        errorEl.innerText = messages.join( ', ' );
+    }*/
+    
+} );
+
+const input = document.querySelector("#nameInput"); // obtiene el input por su id
+
+input.addEventListener('input', function() { // agrega un listener para el evento 'input'
+  const regex = /^[a-zA-ZÁáÉéÍíÓóÚú\s]+$/; // expresión regular que solo acepta letras
+  const valor = this.value; // obtiene el valor actual del input
+
+  if (!regex.test(valor)) { // si el valor no cumple la expresión regular
+      this.value = valor.slice( 0, -1 ); // elimina el último carácter del valor
+    console.log( "Solo letras" );
+    input.setAttribute( 'style', 'color: red;' );
+  }
+} );
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+
+/*//const email = document.querySelector('#email').value.trim();
 //const password = document.querySelector('#password').value.trim();
-
-  // Validacion errores
+// Validacion errores
 let errors = [];
 if (name === '') {
-    errors.push('Nombre completo, por favor');
-}
+    errors.push("Nombre completo, por favor");
+}*/
 
 /*
 if (email === '') {
@@ -49,7 +89,7 @@ if (email === '') {
 } else if (password.length < 8) {
     errors.push('Password must be at least 8 characters long');
 }*/
-    
+    /*
 if (password === '') {
     errors.push('Password is required');
 } else if (password.length < 8) {
@@ -73,8 +113,7 @@ errorList.appendChild(errorItem);
 } else {
     // Los valores son válidos, puedes procesarlos aquí
     alert('Form submitted');
-}
-});
+}*/
 
 /*function isValidEmail(email) {
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
